bef5ac0b31e04ab7ee1a249dffce679bc1fb8cf3
dai <d.sugata.sys23@morijyobi.ac.jp>
2023-11-10 09:56:30 +0900

    ensyu

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Optional;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("身長を入力してください。");
+        double height = scanner.nextDouble();
+
+        System.out.println("体重を入力してください。");
+        double weight = scanner.nextDouble();
+        double BMI = weight / (height*height);
+
+        System.out.println(String.format("%.2f", BMI));
+    }
+
+}
