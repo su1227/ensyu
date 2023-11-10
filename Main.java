6bf2fe7f4cd22a00c646e1c8a999eef29bc7491d
Dai Sugata <146404574+su1227@users.noreply.github.com>
2023-11-10 11:23:46 +0900

    Add files via upload
    間違いのコミットメッセージ

--- Main.java
+++ Main.java
@@ -12,6 +12,15 @@ public class Main {
         double BMI = weight / (height*height);
 
         System.out.println(String.format("%.2f", BMI));
+
+        System.out.println("身長を入力してください。");
+        double height2 = scanner.nextDouble();
+
+        System.out.println("体重を入力してください。");
+        double weight2 = scanner.nextDouble();
+        double BMI2 = weight / (height*height);
+
+        System.out.println(String.format("%.2f", BMI2));
     }
 
 }
