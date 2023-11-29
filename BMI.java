7cde7768fb5d8df38010f72a4e6dd6ba8a49bce8
Dai Sugata <146404574+su1227@users.noreply.github.com>
2023-11-29 12:44:41 +0900

    修正

--- BMI.java
+++ BMI.java
@@ -1,26 +1,26 @@
-import java.util.Optional;
 import java.util.Scanner;
-public class Main {
+
+public class BMI {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("身長を入力してください。");
-        double height = scanner.nextDouble();
+        System.out.println("1人目の身長を入力してください。");
+        double height1 = scanner.nextDouble();
 
-        System.out.println("体重を入力してください。");
-        double weight = scanner.nextDouble();
-        double BMI = weight / (height*height);
+        System.out.println("1人目の体重を入力してください。");
+        double weight1 = scanner.nextDouble();
+        double BMI1 = weight1 / (height1 * height1);
 
-        System.out.println(String.format("%.2f", BMI));
+        System.out.println("1人目のBMIは: " + String.format("%.2f", BMI1) + "です。");
 
-        System.out.println("身長を入力してください。");
+
+        System.out.println("2人目の身長を入力してください。");
         double height2 = scanner.nextDouble();
 
-        System.out.println("体重を入力してください。");
+        System.out.println("2人目の体重を入力してください。");
         double weight2 = scanner.nextDouble();
-        double BMI2 = weight / (height*height);
+        double BMI2 = weight2 / (height2 * height2);
 
-        System.out.println(String.format("%.2f", BMI2));
+        System.out.println("2人目のBMIは: " + String.format("%.2f", BMI2) + "です。");
     }
-
 }
